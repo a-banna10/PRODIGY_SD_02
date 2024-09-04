6a106ccd877627a5a06a33b2c0ce4871b487ea27
LENOVO <LENOVO@DESKTOP-1Q7JF0S>
2024-09-04 17:57:32 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PRODIGY_SD_02
+{
+    partial class GuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Yellow;
+            this.lblTitle.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(271, 97);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(256, 38);
+            this.lblTitle.TabIndex = 11;
+            this.lblTitle.Text = "Guess the Number";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Font = new System.Drawing.Font("Palatino Linotype", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFeedback.ForeColor = System.Drawing.Color.White;
+            this.lblFeedback.Location = new System.Drawing.Point(260, 300);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(0, 27);
+            this.lblFeedback.TabIndex = 10;
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.BackColor = System.Drawing.Color.Yellow;
+            this.lblAttempts.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAttempts.Location = new System.Drawing.Point(342, 327);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(117, 27);
+            this.lblAttempts.TabIndex = 9;
+            this.lblAttempts.Text = "Attempts: 0\r\n";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(341, 246);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(118, 36);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.BackColor = System.Drawing.Color.White;
+            this.txtGuess.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(341, 210);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(118, 30);
+            this.txtGuess.TabIndex = 7;
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter your guess between 1 and 100:";
+            // 
+            // GuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PRODIGY_SD_02.Properties.Resources.stylish_question_mark_sing_yellow_background_with_text_space_1017_43318;
+            this.ClientSize = new System.Drawing.Size(718, 498);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label1);
+            this.Name = "GuessingGame";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PRODIGY_SD_02
+{
+    public partial class GuessingGame : Form
+    {
+        public GuessingGame()
+        {
+            InitializeComponent();
+        }
+        int number=new Random().Next(1,101);
+        int count = 0;
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int guess = int.Parse(txtGuess.Text);
+
+                count = count + 1;
+                if (guess < number)
+                {
+                    lblFeedback.Text = "Your guess is too low! Try again";
+                }
+                else if (guess > number)
+                {
+                    lblFeedback.Text = "Your guess is too high! Try again";
+                }
+                else
+                {
+                    lblFeedback.Text = "Your guess is Correct! Congratulations";
+                    btnSubmit.Enabled = false;
+                }
+                lblAttempts.Text = "Attempts:" + count;
+            }
+            catch (FormatException) { MessageBox.Show("Invalid input! Enter a number"); }
+        }
+    }
+ }
